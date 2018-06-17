2abc731d2a1c5b351062f32076cb72a9552783f1
aXionDevelopment <axe.mail@gmail.com>
2018-06-17 06:00:22 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("running..");
+    pinMode(13, OUTPUT);
+}
+
+
+void loop()
+{
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(400);
+    Serial.println(millis());
+}
